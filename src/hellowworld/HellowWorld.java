8628eb2358ded5b89c5ba107567e467b35f9e415
1056661516 <33505540+1056661516@users.noreply.github.com>
2024-03-07 11:18:45 +0800

    Add files via upload

---
+++ src/hellowworld/HellowWorld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellowworld;
+
+/**
+ *
+ * @author g12911hh
+ */
+public class HellowWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.println("おはよう世界");
+    }
+}
